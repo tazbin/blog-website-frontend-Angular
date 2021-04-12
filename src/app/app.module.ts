ff4f034ab6c98a0ccd64fa3634d8169942684b3d
tazbin <md.tazbinur.rahaman@gmail.com>
2021-04-12 23:28:37 +0600

    project developed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AllBlogsComponent } from './all-blogs/all-blogs.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { ReverseArrayPipe } from './pipes/reverse-array.pipe';
+import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { SignupComponent } from './signup/signup.component';
 import { WriteBlogComponent } from './write-blog/write-blog.component';
-import { BlogComponent } from './blog/blog.component';
-import { AllBlogsComponent } from './all-blogs/all-blogs.component';
-import { ReverseArrayPipe } from './pipes/reverse-array.pipe';
-import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { ContactComponent } from './contact/contact.component';
-
 
 @NgModule({
   declarations: [
